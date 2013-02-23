ea35f324ba625a8d913fba69f017fc409c33ced3
Andrew Perrins <andrew@ajperrins.com>
2013-02-23 13:26:37 -0500

    1

--- dist/scripts/5b41f0a3.main.js
+++ dist/scripts/5b41f0a3.main.js
@@ -2,11 +2,7 @@
 
   var serverUrl = 'http://govathon.tagping.com/';
   var dataBase =
-    [{ "id": 8, "name": "City Hall", "address_street": "55 Trinity Street", "address_zip": 30303, "area": 15000, "cost": 200, "description": "This is the city hall of Atlanta", "phone": "5555555555", "contact_name": "John Doe", "email": "john@doe.com", "created_at": "0000-00-00 00:00:00", "updated_at": "0000-00-00 00:00:00", "latitude": "33.74895280", "longitude": "-84.39049340" },
-      { "id": 9, "name": "Trinity Church", "address_street": "265 Washington Street", "address_zip": 30303, "area": 5000, "cost": 520, "description": "This is Trinity United Methodist Church", "phone": "3333333333", "contact_name": "Jim Smith", "email": "jim@smith.com", "created_at": "0000-00-00 00:00:00", "updated_at": "0000-00-00 00:00:00", "latitude": "33.74253220", "longitude": "-84.39047680" },
-      { "id": 11, "name": "The Tabernacle", "address_street": "152 Luckie Street NW", "address_zip": 30303, "area": 150000, "cost": 10, "description": "This is the Tabernacle", "phone": "4444444444", "contact_name": "Bill Washington", "email": "bill@washington.com", "created_at": "0000-00-00 00:00:00", "updated_at": "0000-00-00 00:00:00", "latitude": "33.75892730", "longitude": "-84.39144250" }];
-
-  //[{ "id": 1, lat: 33.7, lng: -84.3, "name": "City Hall", "address_street": "55 Trinity Street", "address_zip": 30303, "area": 15000, "cost": 200, "description": "This is the city hall of Atlanta", "phone": 5555555555, "contact_name": "John Doe", "email": "john@doe.com", "created_at": "0000-00-00 00:00:00", "updated_at": "0000-00-00 00:00:00" },
+    [{ "id": 8, "name": "City Hall", "address_street": "55 Trinity Street", "address_zip": 30303, "area": 15000, "cost": 200, "description": "This is the city hall of Atlanta", "phone": 5555555555, "contact_name": "John Doe", "email": "john@doe.com", "created_at": "0000-00-00 00:00:00", "updated_at": "0000-00-00 00:00:00", "latitude": 33.7489528, "longitude": -84.3904934 }, { "id": 9, "name": "Trinity Church", "address_street": "265 Washington Street", "address_zip": 30303, "area": 5000, "cost": 520, "description": "This is Trinity United Methodist Church", "phone": 3333333333, "contact_name": "Jim Smith", "email": "jim@smith.com", "created_at": "0000-00-00 00:00:00", "updated_at": "0000-00-00 00:00:00", "latitude": 33.7425322, "longitude": -84.3904768 }, { "id": 10, "name": "Hypepotamus", "address_street": "817 W Peachtree Street NW", "address_zip": 30308, "area": 200, "cost": 20000, "description": "This is Hypepotamus", "phone": 1111111111, "contact_name": "Sue Jones", "email": "sue@jones.com", "created_at": "0000-00-00 00:00:00", "updated_at": "0000-00-00 00:00:00", "latitude": 33.778295380292, "longitude": -84.385792219709 }, { "id": 11, "name": "The Tabernacle", "address_street": "152 Luckie Street NW", "address_zip": 30303, "area": 150000, "cost": 10, "description": "This is the Tabernacle", "phone": 4444444444, "contact_name": "Bill Washington", "email": "bill@washington.com", "created_at": "0000-00-00 00:00:00", "updated_at": "0000-00-00 00:00:00", "latitude": 33.7589273, "longitude": -84.3914425 }, { "id": 12, "name": "Starbucks", "address_street": "21 14th Sreet NE", "address_zip": 30309, "area": 1700, "cost": 2200, "description": "This is the coffee place", "phone": 8888888888, "contact_name": "John Coffee", "email": "joe@coffee.com", "created_at": "0000-00-00 00:00:00", "updated_at": "0000-00-00 00:00:00", "latitude": 33.788021630292, "longitude": -84.386101019708 }];  //[{ "id": 1, lat: 33.7, lng: -84.3, "name": "City Hall", "address_street": "55 Trinity Street", "address_zip": 30303, "area": 15000, "cost": 200, "description": "This is the city hall of Atlanta", "phone": 5555555555, "contact_name": "John Doe", "email": "john@doe.com", "created_at": "0000-00-00 00:00:00", "updated_at": "0000-00-00 00:00:00" },
   //              { "id": 2, lat: 33.73, lng: -84.33, "name": "City Wall", "address_street": "101 Main", "address_zip": 90210, "area": 15000, "cost": 150, "description": "This is the city hall of Atlanta", "phone": 5555555555, "contact_name": "Mark Twain", "email": "mark.twain@gmail.com", "created_at": "0000-00-00 00:00:00", "updated_at": "0000-00-00 00:00:00" }];
 
 
@@ -42,11 +38,12 @@
 
       $http({
         method: 'GET',
-        url: serverUrl + '/facilities'
+        url: serverUrl + 'facilities'
       })
         .success(
         function (data) {
           $scope.facilities = data;
+          updateMarkers($scope.facilities);
         })
         .error(function (data, status, headers, config) {
           alert('error retrieving');
